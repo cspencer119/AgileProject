e3c81dcca11c17edfbfb05ba1603117d9a520a7c
Cyrus Spencer <cyrus.spencer119@gmail.com>
2022-04-06 11:35:49 -0400

    Animal Detail Class

--- AnimalModel/AnimalDetail.cs
+++ AnimalModel/AnimalDetail.cs
@@ -8,5 +8,10 @@ namespace AnimalModel
 {
     public class AnimalDetail
     {
+        public int AnimalId { get; set; }
+        public string NameCommon { get; set; }
+        public string NameScientific { get; set; }
+        public string FunFacts { get; set; }
+
     }
 }
