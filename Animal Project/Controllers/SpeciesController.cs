d52b47992bea043f1f3885fd6399d78b8bb0bab8
Ethan Starling <ethan.jr.starling@gmail.com>
2022-04-06 10:40:00 -0400

    added species controller and createspiecesservice method

---
+++ Animal Project/Controllers/SpeciesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Animal_Project.Controllers
+{
+    public class SpeciesController : ApiController
+    {
+    }
+}
