a55c73ce9b2e9717e165335891f536de21dce55c
Ethan Starling <ethan.jr.starling@gmail.com>
2022-04-06 14:02:31 -0400

    Added in createspecies method

--- AnimalService/SpeciesService.cs
+++ AnimalService/SpeciesService.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Animal_Project.Models;
 
 namespace AnimalService
 {
     public class SpeciesService
     {
+        public bool CreateSpecies(SpeciesCreate model)
+        {
+            var entity =
+                new Species()
+                {
+                    SpeciesId = model.CategoryId,
+                    SpeciesName = model.CategoryName
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Species.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
     }
 }
